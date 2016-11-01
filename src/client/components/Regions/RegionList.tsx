c89d5c129b4cc023d2380d9bcb94c97afda056dd
Michael Heilmann <mheilman@ist.ucf.edu>
2016-11-01 11:43:20 -0400

    Created a modal view for region management

--- src/client/components/Regions/RegionList.tsx
+++ src/client/components/Regions/RegionList.tsx
@@ -6,22 +6,53 @@ import { RegionView } from './RegionView';
 import { Estate } from '../Estates';
 import { Region } from '.';
 
+import { ManageModal } from './Manage';
+
 import { Grid, Row, Col } from 'react-bootstrap';
 
-interface regionProps {
+interface props {
     dispatch: (a: Action) => void,
     regions: Map<string, Region>,
     estateMap: Map<string, number>,
     estates: Map<number, Estate>
 }
 
-export class RegionList extends React.Component<regionProps, {}> {
+export class RegionList extends React.Component<props, {}> {
+    state: {
+        showManage: boolean
+        selectedRegion: Region
+    }
+
+    constructor(p: props){
+        super(p);
+        this.state = {
+            showManage: false,
+            selectedRegion: null
+        }
+    }
+
+    onManageRegion(r: Region) {
+        this.setState({
+            showManage: true,
+            selectedRegion: r
+        })
+    }
+
+    dismissManage(){
+        this.setState({
+            showManage: false
+        })
+    }
 
     render() {
         let regions = this.props.regions.toList().map((r: Region) => {
             let estateId: number = this.props.estateMap.get(r.uuid);
             let e = this.props.estates.get(estateId);
-            return <RegionView key={r.uuid} region={r} estate={e ? e : null}/>
+            return <RegionView
+                key={r.uuid}
+                region={r}
+                estate={e ? e : null}
+                onManage={this.onManageRegion.bind(this, r)} />
         })
 
         return (
@@ -33,6 +64,7 @@ export class RegionList extends React.Component<regionProps, {}> {
                     <Col md={3}>Host</Col>
                 </Row>
                 {regions}
+                { this.state.showManage ? <ManageModal dismiss={this.dismissManage.bind(this)} region={this.state.selectedRegion}/> : <span />}
             </Grid>
         )
     }
--- src/client/components/Regions/RegionView.tsx
+++ src/client/components/Regions/RegionView.tsx
@@ -3,11 +3,12 @@ import { Store } from 'redux'
 import { Estate } from '../Estates';
 import { Region } from '.';
 
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Button } from 'react-bootstrap';
 
 interface regionProps {
   region: Region,
-  estate: Estate
+  estate: Estate,
+  onManage: () => void
 }
 
 export class RegionView extends React.Component<regionProps, {}> {
@@ -15,9 +16,12 @@ export class RegionView extends React.Component<regionProps, {}> {
   render() {
     return (
       <Row>
-        <Col md={3}>{this.props.region.name}</Col>
-        <Col md={3}>X: {this.props.region.locX} Y: {this.props.region.locY}</Col>
-        <Col md={3}>{this.props.estate ? this.props.estate.EstateName : '~' }</Col>
+        <Col md={1}><Button bsSize='xsmall' onClick={this.props.onManage} >
+          <i className="fa fa-cog" aria-hidden="true"></i>
+        </Button></Col>
+        <Col md={2}>{this.props.region.name}</Col>
+        <Col md={3}>X: {this.props.region.locX}Y: {this.props.region.locY}</Col>
+        <Col md={3}>{this.props.estate ? this.props.estate.EstateName : '~'}</Col>
         <Col md={3}>{this.props.region.slaveAddress}</Col>
       </Row>
     )
